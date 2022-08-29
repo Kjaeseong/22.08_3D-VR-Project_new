1fdc6aedef22b158e0f8fe5de9c4b863c6f3b97d
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-08-29 09:35:43 +0900

    Feat: Create File Manager

--- Assets/Scripts/FileManager.cs
+++ Assets/Scripts/FileManager.cs
@@ -6,21 +6,18 @@ public class FileManager : SingletonBehaviour<FileManager>
 {
     public Sprite Image;
     public List<Sprite> ImageList;
-    public int TotalItem; 
+    public int TotalItem;
 
-    //public List<Dictionary<string, object>> CSV = new List<Dictionary<string, object>>();
-
-
+    List<Dictionary<string, object>> CSV = new List<Dictionary<string, object>>();
 
 
 
-    // Start is called before the first frame update
+
+
     private void Start()
     {
-        Debug.Log("File Manager Awake");
-
-        //CSV = CSVReader.Read("Item/ItamData");
-        Debug.Log("CSV Reading");
+        CSV = CSVReader.Read("Item/ItemData");
+        TotalItem = CSV.Count;
 
     }
 
